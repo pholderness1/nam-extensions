6027e8e4e587a83365fdaa5baad43e44b81f7628
Sebastiaan Veldhuisen <sveldhuisen@idfocus.nl>
2017-09-28 19:27:10 +0200

    Updated addCookie to set cookieDomain and cookiePath to the response

--- nam-util/src/main/java/nl/idfocus/nam/util/CookieUtils.java
+++ nam-util/src/main/java/nl/idfocus/nam/util/CookieUtils.java
@@ -10,39 +10,47 @@ import javax.servlet.http.HttpServletResponse;
 public abstract class CookieUtils {
 
 	public static final String DEFAULT_COOKIE_PATH = "/";
-	
-	public static void addCookie(HttpServletResponse response, Cookie cookie, Integer maxAge, Boolean secure) {
+
+	public static void addCookie(HttpServletResponse response, Cookie cookie, Integer maxAge, Boolean secure,
+			String domain, String path) {
 		if (maxAge != null) {
 			cookie.setMaxAge(maxAge);
 		}
-		
+
 		if (secure != null) {
 			cookie.setSecure(secure);
 		}
-		
-		response.addCookie(cookie);
-	}	
-	
-	public static Cookie createCookie(String name, String value, String domain, String path) {
-		final Cookie cookie = new Cookie(name, value);
-		
+
 		if (domain != null) {
 			cookie.setDomain(domain);
 		}
-		
-		if (path == null) {
-			cookie.setPath(CookieUtils.DEFAULT_COOKIE_PATH);
-		}
-		else {
+
+		if (path != null) {
 			cookie.setPath(path);
 		}
-		
-		return cookie;		
+
+		response.addCookie(cookie);
 	}
-	
+
+	public static Cookie createCookie(String name, String value, String domain, String path) {
+		final Cookie cookie = new Cookie(name, value);
+
+		if (domain != null) {
+			cookie.setDomain(domain);
+		}
+
+		if (path == null) {
+			cookie.setPath(CookieUtils.DEFAULT_COOKIE_PATH);
+		} else {
+			cookie.setPath(path);
+		}
+
+		return cookie;
+	}
+
 	public static Cookie getCookie(HttpServletRequest request, String name) {
 		final Cookie cookies[] = request.getCookies();
-		
+
 		if (cookies != null) {
 			for (final Cookie cookie : cookies) {
 				if (name.equals(cookie.getName())) {
@@ -50,7 +58,7 @@ public abstract class CookieUtils {
 				}
 			}
 		}
-		
+
 		return null;
 	}
 
